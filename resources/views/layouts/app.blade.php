b6ce570be96326a514d84f0124cfa8a67a887b25
Diana Laura de la Vega Sierra <blackpyjac@protonmail.com>
2023-05-19 17:08:34 -0600

    new changes in the post view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,7 +38,7 @@
                                 id="menu">
                                 <li class="nav-item">
                                     <a href="#" class="nav-link align-middle px-0 text-decoration-none text-black">
-                                        <i class="fs-3 bi bi-house-door-fill"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
+                                        <i class="fs-3 bi bi-house-door"></i> <span class="ms-1 d-none d-sm-inline">Home</span>
                                     </a>
                                 </li>
                                 <li>
@@ -48,56 +48,21 @@
                                 </li>
                                 <li>
                                     <a href="#" class="nav-link px-0 align-middle text-decoration-none text-black">
-                                        <i class="fs-4 bi bi-compass"></i> <span
-                                            class="ms-1 d-none d-sm-inline">Explore</span></a>
-                                </li>
-                           
-                                <li>
-                                    <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                                        <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span>
-                                    </a>
-                                    <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                                        <li class="w-100">
-                                            <a href="#" class="nav-link px-0"> <span
-                                                    class="d-none d-sm-inline">Product</span> 1</a>
-                                        </li>
-                                        <li>
-                                            <a href="#" class="nav-link px-0"> <span
-                                                    class="d-none d-sm-inline">Product</span> 2</a>
-                                        </li>
-                                        <li>
-                                            <a href="#" class="nav-link px-0"> <span
-                                                    class="d-none d-sm-inline">Product</span> 3</a>
-                                        </li>
-                                        <li>
-                                            <a href="#" class="nav-link px-0"> <span
-                                                    class="d-none d-sm-inline">Product</span> 4</a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li>
-                                    <a href="#" class="nav-link px-0 align-middle">
-                                        <i class="fs-4 bi-people"></i> <span
-                                            class="ms-1 d-none d-sm-inline">Customers</span> </a>
+                                        <img src="{{Auth()->user()->profile->profileImage()}}" alt="hugenerd" width="24" height="24"
+                                        class="rounded-circle"> <span
+                                            class="ms-1 d-none d-sm-inline">Profile</span></a>
                                 </li>
                             </ul>
                             <hr>
                             <div class="dropdown pb-4">
                                 <a href="#"
-                                    class="d-flex align-items-center text-black text-decoration-none dropdown-toggle"
+                                    class="d-flex align-items-center text-black text-decoration-none "
                                     id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30"
-                                        class="rounded-circle">
-                                    <span class="d-none d-sm-inline mx-1">loser</span>
+                                    <i class="fs-3 bi bi-list"></i>
+                                    <span class="ms-1 d-none d-sm-inline">More</span>
                                 </a>
-                                <ul class="dropdown-menu dropdown-menu-dark text-small shadow"
+                                <ul class="dropdown-menu text-small shadow"
                                     aria-labelledby="dropdownUser1">
-                                    <li><a class="dropdown-item" href="#">New project...</a></li>
-                                    <li><a class="dropdown-item" href="#">Settings</a></li>
-                                    <li><a class="dropdown-item" href="#">Profile</a></li>
-                                    <li>
-                                        <hr class="dropdown-divider">
-                                    </li>
                                     <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
                                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                             @csrf
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,39 +1,91 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        @if ($posts->count() > 0)
-            @foreach ($posts as $post)
-                <div class="row">
-                    <div class="col-6 offset-3">
-                        <a href="/profile/{{ $post->user->id }}">
-                            <img src="/storage/{{ $post->image }}" class="w-100">
-                        </a>
-                    </div>
-                </div>
-                <div class="row pt-2 pb-4">
-                    <div class="col-6 offset-3">
-                        <div>
-                            <p>
-                                <span class="fw-bold">
-                                    <a href="/profile/{{ $post->user->id }}" class=" text-decoration-none">
-                                        <span class="text-dark">{{ $post->user->username }}</span>
-                                    </a>
-                                </span> {{ $post->caption }}
-                            </p>
+    <div class="container text-center mt-3">
+        <div class="row">
+            <div class="col-md-8 col-sm-12">
+                @if ($posts->count() > 0)
+                    @foreach ($posts as $post)
+                        <div class="row">
+                            <div class="col-6 offset-3">
+                                <a href="/profile/{{ $post->user->id }}">
+                                    <img src="/storage/{{ $post->image }}" class="w-100">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="row pt-2 pb-4">
+                            <div class="col-6 offset-3">
+                                <div>
+                                    <p>
+                                        <span class="fw-bold">
+                                            <a href="/profile/{{ $post->user->id }}" class=" text-decoration-none">
+                                                <span class="text-dark">{{ $post->user->username }}</span>
+                                            </a>
+                                        </span> {{ $post->caption }}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                    <div class="row">
+                        <div class="col-12 d-flex justify-content-center">
+                            {{ $posts->links() }}
                         </div>
                     </div>
-                </div>
-            @endforeach
-            <div class="row">
-                <div class="col-12 d-flex justify-content-center">
-                    {{ $posts->links() }}
+                @else
+                    <div class="card  bg-light mt-3">
+                        <div class="card-body mt-5">
+                            <div class="row">
+                                <div class="col-12 d-flex justify-content-center ">
+                                    <div class="border border-3 border-dark rounded-circle "
+                                        style="width: 145px; height:145px;">
+                                        <div class="mt-3">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="95" height="95"
+                                                fill="currentColor" class="bi bi-camera" viewBox="0 0 16 16">
+                                                <path
+                                                    d="M15 12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1h1.172a3 3 0 0 0 2.12-.879l.83-.828A1 1 0 0 1 6.827 3h2.344a1 1 0 0 1 .707.293l.828.828A3 3 0 0 0 12.828 5H14a1 1 0 0 1 1 1v6zM2 4a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2h-1.172a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 9.172 2H6.828a2 2 0 0 0-1.414.586l-.828.828A2 2 0 0 1 3.172 4H2z" />
+                                                <path
+                                                    d="M8 11a2.5 2.5 0 1 1 0-5 2.5 2.5 0 0 1 0 5zm0 1a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7zM3 6.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z" />
+                                            </svg>
+
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                                <div class="col-md-12 text-center">
+                                    <div class="fs-1 fw-medium">No Posts Yet</div>
+                                </div>
+                            </div>
+
+
+                        </div>
+
+                    </div>
+                @endif
+            </div>
+            <div class="col-md-4 d-sm-none d-md-block d-md-none d-lg-block">
+                <div class="list-group">
+                    <a href="/profile/{{ auth()->user()->id }}" class="list-group-item list-group-item-action d-flex justify-content-start align-items-center bg-light border border-0">
+                        <img src="{{ auth()->user()->profile->profileImage() }}" alt="hugenerd" width="60"
+                            height="60" class="rounded-circle">
+                        <div class="ps-2">
+                            <div class="fw-bold fs-6"><small>{{ auth()->user()->username }}</small></div>
+                            <div class="fs-6 text-muted">{{ auth()->user()->name }}</div>
+                        </div>
+                    </a>
+                    <div class="list-group-item  d-flex justify-content-between align-items-center bg-light border border-0 text-muted">
+                        <div class="fw-bold fs-6">Suggested for you</div>
+                        <a href="#" class="text-decoration-none fw-bold text-black"><small>See All</small></a>
+                    </div>
+                    <div href="" class="list-group-item list-group-item-action  d-flex justify-content-between align-items-center bg-light border border-0">
+                        <img src="{{ auth()->user()->profile->profileImage() }}" alt="hugenerd" width="35"
+                            height="35" class="rounded-circle">
+                        <span class="badge bg-primary rounded-pill">1</span>
+                    </div>
                 </div>
             </div>
-        @else
-            <h1>test</h1>
-            <i class="bi-alarm"></i>
+        </div>
 
-        @endif
     </div>
 @endsection
